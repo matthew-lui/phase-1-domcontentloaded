cb00b79e1ff48e229bfe7bab7c71427d5a0d1a1b
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-09-26 09:23:36 -0400

    commit tree ae6eccc5c5efec513ba37a41e4a29ca83f781b27

--- test/indexTest.js
+++ test/indexTest.js
@@ -14,3 +14,27 @@ describe( "index.js", () => {
   } )
 
 } )
+
+
+// document. addEventListener ( 'DOMContentLoaded', () => {
+//   let form = document.querySelector( 'form')
+//   form. addEventListener( 'submit', (e)
+//   -s
+//   {
+//   e.preventDefault()
+//   buildToDo(e. target.new todo.value)
+//   form. reset 0)
+//   })
+//   })
+//   function buildToDo(todo){
+//   let D = document.createElement('p')
+//   let btn = document.createElement( 'button')
+//   btn.addEventListener( 'click', handleDelete)
+//   btn.textContent = 'x'
+//   p.textContent =
+//   "${todo}
+//   p.appendChild(btn)
+//   console. log (p)
+//   document.querySelector ('#todo container' ).appendChild(p)
+//   function handleDelete(e){
+//   e.target.parentNode. remove
